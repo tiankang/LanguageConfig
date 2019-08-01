74cb7c897eda8e246f913ea30aa48bcca34282d3
tiankang <495536461@qq.com>
2019-08-01 23:59:56 +0800

    首次提交测试

---
+++ LanguageConfig/Assets/Scripts/LanguageText.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LanguageText : Text
+{
+    /// <summary>
+    ///对应表里的ID
+    /// </summary>
+    public uint mLanguageId;
+
+
+}
